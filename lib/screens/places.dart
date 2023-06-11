de2f80ea40d7883d5c09633afcd6bb19bfcc77dd
Mohamed abo el maaty zaky <98196426+MohamedAboElM3aTy@users.noreply.github.com>
2023-06-12 02:50:59 +0300

    Reflecting the new place

---
+++ lib/screens/places.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'package:favourite_places_learning/widgets/places_list.dart';
+import 'package:favourite_places_learning/screens/add_place.dart';
+import 'package:favourite_places_learning/providers/user_places.dart';
+
+class PlacesScreen extends ConsumerWidget {
+  const PlacesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final userPlaces = ref.watch(userPlacesProvider);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your places'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (ctx) => const AddPlaceScreen(),
+                ),
+              );
+            },
+            icon: const Icon(Icons.add),
+          ),
+        ],
+      ),
+      body: PlacesList(
+        places: userPlaces,
+      ),
+    );
+  }
+}
