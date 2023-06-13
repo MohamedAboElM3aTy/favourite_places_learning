b2687373fc0b5a83018ace9bccbf99ba7206919e
Mohamed abo el maaty zaky <98196426+MohamedAboElM3aTy@users.noreply.github.com>
2023-06-13 19:23:46 +0300

    receiving and sending imageFile

--- lib/providers/user_places.dart
+++ lib/providers/user_places.dart
@@ -1,11 +1,14 @@
-import 'package:favourite_places_learning/models/place.dart';
+import 'dart:io';
+
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'package:favourite_places_learning/models/place.dart';
 
 class UserPlacesNotifier extends StateNotifier<List<Place>> {
   UserPlacesNotifier() : super(const []);
 
-  void addPlace(String title) {
-    final newPlace = Place(title: title);
+  void addPlace(String title, File image) {
+    final newPlace = Place(title: title, image: image);
     state = [newPlace, ...state];
   }
 }
