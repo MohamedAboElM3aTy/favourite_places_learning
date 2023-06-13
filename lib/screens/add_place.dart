8fb1f57755c733d27ad8ef124aa8be61b82cf64f
Mohamed abo el maaty zaky <98196426+MohamedAboElM3aTy@users.noreply.github.com>
2023-06-13 19:24:47 +0300

    Adding imageInput widget

--- lib/screens/add_place.dart
+++ lib/screens/add_place.dart
@@ -1,6 +1,10 @@
-import 'package:favourite_places_learning/providers/user_places.dart';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'package:favourite_places_learning/providers/user_places.dart';
+import 'package:favourite_places_learning/widgets/image_input.dart';
 
 class AddPlaceScreen extends ConsumerStatefulWidget {
   const AddPlaceScreen({super.key});
@@ -11,15 +15,18 @@ class AddPlaceScreen extends ConsumerStatefulWidget {
 
 class _AddPlaceScreenState extends ConsumerState<AddPlaceScreen> {
   final _titleController = TextEditingController();
+  File? _selectedImage;
 
   void _savePlace() {
     final enteredTitle = _titleController.text;
 
-    if (enteredTitle.isEmpty) {
+    if (enteredTitle.isEmpty || _selectedImage == null) {
       return;
     }
 
-    ref.read(userPlacesProvider.notifier).addPlace(enteredTitle);
+    ref
+        .read(userPlacesProvider.notifier)
+        .addPlace(enteredTitle, _selectedImage!);
     // ? i add .notifier , to read the function inside my provider
 
     Navigator.of(context).pop();
@@ -48,6 +55,10 @@ class _AddPlaceScreenState extends ConsumerState<AddPlaceScreen> {
                 fontSize: 16,
                 color: Theme.of(context).colorScheme.onBackground,
               ),
+            ),
+            const SizedBox(height: 10),
+            ImageInput(
+              onPickImage: (image) => _selectedImage = image,
             ),
             const SizedBox(height: 16),
             ElevatedButton.icon(
