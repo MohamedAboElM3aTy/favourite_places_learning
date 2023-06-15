45e1f9ecc3b37a0916f8ada0d377d1a972139414
Mohamed abo el maaty zaky <98196426+MohamedAboElM3aTy@users.noreply.github.com>
2023-06-16 01:24:50 +0300

    Define a bluePrint for loaction

--- lib/models/place.dart
+++ lib/models/place.dart
@@ -4,10 +4,27 @@ import 'package:uuid/uuid.dart';
 
 const uuid = Uuid();
 
+class PlaceLocation {
+  final double latitude;
+  final double longitude;
+  final String address;
+
+  PlaceLocation({
+    required this.latitude,
+    required this.longitude,
+    required this.address,
+  });
+}
+
 class Place {
-  Place({required this.title, required this.image}) : id = uuid.v4();
+  Place({
+    required this.location,
+    required this.title,
+    required this.image,
+  }) : id = uuid.v4();
 
   final String id;
   final String title;
   final File image;
+  final PlaceLocation location;
 }
