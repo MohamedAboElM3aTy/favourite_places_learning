8c5350bec28b1f2f876216d4c48797af83095398
Mohamed abo el maaty zaky <98196426+MohamedAboElM3aTy@users.noreply.github.com>
2023-06-17 04:00:50 +0300

    Make the class constant

--- lib/models/place.dart
+++ lib/models/place.dart
@@ -9,7 +9,7 @@ class PlaceLocation {
   final double longitude;
   final String address;
 
-  PlaceLocation({
+  const PlaceLocation({
     required this.latitude,
     required this.longitude,
     required this.address,
